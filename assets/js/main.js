9c52a25eb25eeed3d79e7a23bf86ae6c4813865c
Mohamedmarzouk2020 <Mohamed_marzouk@sci.asu.edu.eg>
2022-12-17 09:22:01 +0900

    modify css and js to add tabs

--- assets/js/main.js
+++ assets/js/main.js
@@ -398,4 +398,20 @@
 						$main._show(location.hash.substr(1), true);
 					});
 
+			// added 17-12-2022 to adjust tabs
+					// Show the first tab and hide the rest
+			// $('#tabs-nav li:first-child').addClass('active');
+			$('.tab-content').hide();
+			// $('.tab-content:first').show();
+
+			// Click function
+			$('#tabs-nav li').click(function(){
+			$('#tabs-nav li').removeClass('active');
+			$(this).addClass('active');
+			$('.tab-content').hide();
+			
+			var activeTab = $(this).find('a').attr('href');
+			$(activeTab).fadeIn();
+			return false;
+			});
 })(jQuery);
